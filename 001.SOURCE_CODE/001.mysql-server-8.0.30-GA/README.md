9fab0c2f4e0ece6a3936ad0d66fd11ee083078ea
Wei <1849553578@qq.com>
2022-09-11 13:00:46 +0800

    feat>MySQL>新版本添加

---
+++ 001.SOURCE_CODE/001.mysql-server-8.0.30-GA/README.md
@@ -0,0 +1,9 @@
+# MySQL8.0.30-GA编译&调试
+## 编译环境
+
+---
+## 编译步骤
+&nbsp;&nbsp;详见：  001.SOURCE_CODE/001.mysql-server-8.0.30-GA/build/000.build-scripts/000.MySQL-Build.sh
+
+---
+## 调试步骤
---
+++ 001.SOURCE_CODE/001.mysql-server-8.0.30-GA/build/000.build-scripts/000.MySQL-Build.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# 配置文件目录
+CONFDIR=`pwd`
+
+# 编译目录不存在,则创建
+if [ ! -d "${CONFDIR}/../001.build-output" ];then
+  echo '001.build-output 文件夹不存在,创建'
+  cd ${CONFDIR}/../ && mkdir 001.build-output
+else
+  echo "文件夹已经存在,清理内部内容"
+  rm -rf ${CONFDIR}/../001.build-output/*
+fi
+
+# 进入编译目录
+cd ${CONFDIR}/../001.build-output
+
+# 输出当前目录
+echo '当前目录:' `pwd`
+
+# 生成makefile文件
+./../../mysql-server-5.7/BUILD/compile-pentium-debug-max-no-ndb
+
+# 编译
+make
+# 创建debug时的data目录
+if [ ! -d "${CONFDIR}/../000.build-scripts/data" ];then
+  echo '000.build-scripts/data 文件夹不存在,创建'
+  mkdir ${CONFDIR}/data
+  # 初始化(data文件夹不存在才需要初始化)
+  ./sql/mysqld --defaults-file=${CONFDIR}/sys/my.cnf --user=root --initialize
+else
+  echo "文件夹已经存在"
+fi
+
+# 启动服务
+./sql/mysqld --defaults-file=${CONFDIR}/sys/my.cnf
