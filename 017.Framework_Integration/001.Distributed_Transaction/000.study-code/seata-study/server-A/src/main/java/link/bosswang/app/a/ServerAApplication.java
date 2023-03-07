e795da359da2d169a64c1595fc7ca63aa276c8cf
Wei <1849553578@qq.com>
2023-03-07 21:57:54 +0800

    feat>JDK>使用自编译JDK问题处理

--- 017.Framework_Integration/001.Distributed_Transaction/000.study-code/seata-study/server-A/src/main/java/link/bosswang/app/a/ServerAApplication.java
+++ 017.Framework_Integration/001.Distributed_Transaction/000.study-code/seata-study/server-A/src/main/java/link/bosswang/app/a/ServerAApplication.java
@@ -2,7 +2,6 @@ package link.bosswang.app.a;
 
 import io.seata.spring.annotation.datasource.EnableAutoDataSourceProxy;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
